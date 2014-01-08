0b3963e7ac10a6858884786250f14d3c68a060ef
activequant <ustaudinger@activequant.com>
2014-01-08 18:47:13 +0100

    Improving test scripts

--- test.sh
+++ test.sh
@@ -1,3 +1,28 @@
-iverilog -o target/tb_main src/main.v src/tb_main.v
-vvp target/tb_main
+
+rm -rf target
+mkdir target
+
+# generic test function for running a verilog test class
+function runtest {
+  echo "Running test bench >>$1<<"
+  testOutFile="target/output_$1"
+  testOutFileLog="target/output_$1.log"
+  iverilog -o $testOutFile $2
+  vvp $testOutFile > $testOutFileLog
+  errorFound=`grep "Error" $testOutFileLog`
+  echo $errorFound
+}
+
+
+echo "=========== Test bench runner ============="
+echo "Running tests ... "
+
+
+
+# now, let's run the test cases. 
+runtest "tb_main" "src/main.v src/tb_main.v"
+
+
+echo "================= Done ===================="
+
 
